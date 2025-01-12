367221e8fd338bd9606efd30c96ccf7d99a7dc95
woragis <masteringthecode.woragis+github@gmail.com>
2025-01-12 01:30:14 -0300

    Create react query hooks and types

---
+++ web/src/hooks/useTodos.ts
@@ -0,0 +1,61 @@
+// src/hooks/useProducts.ts
+import { useQuery } from "@tanstack/react-query";
+
+// src/hooks/useProductMutations.ts
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import api from "../api/axiosInstance";
+import { UpdateTodo, Todo, CreateTodo } from "../types/todo.types";
+
+export const useTodos = () => {
+  const { isLoading, error, data } = useQuery<Todo[]>({
+    queryKey: ["todos"],
+    queryFn: async () => {
+      const { data } = await api.get<Todo[]>("/todos");
+      return data;
+    },
+  });
+  return { isLoading, error, data };
+};
+
+export const useTodoMutations = () => {
+  const queryClient = useQueryClient();
+
+  const addTodo = useMutation<
+    Todo,
+    Error,
+    Omit<Todo, "id" | "created_at" | "updated_at">
+  >({
+    mutationFn: async (newTodo: CreateTodo) => {
+      const { data } = await api.post<Todo>("/todos", newTodo);
+      return data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: "todos" });
+    },
+  });
+
+  const updateTodo = useMutation<Todo, Error, Todo>({
+    mutationFn: async (updatedTodo: UpdateTodo) => {
+      const { data } = await api.put<Todo>(
+        `/todos/${updatedTodo.id}`,
+        updatedTodo
+      );
+      return data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: "todos" });
+    },
+  });
+
+  const deleteTodo = useMutation({
+    mutationFn: async (id: number) => {
+      const deleted = await api.delete<boolean>(`/todos/${id}`);
+      return deleted;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: "todos" });
+    },
+  });
+
+  return { addTodo, updateTodo, deleteTodo };
+};
---
+++ web/src/types/todo.types.ts
@@ -0,0 +1,21 @@
+export interface Todo {
+  id: number;
+  title: string;
+  content: string;
+  author_id: number;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface CreateTodo {
+  title: string;
+  content: string;
+  author_id: number;
+}
+
+export interface UpdateTodo {
+  id: number;
+  title: string;
+  content: string;
+  author_id: number;
+}
