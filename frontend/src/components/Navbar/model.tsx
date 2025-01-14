70966942b8df64dcbdf1820e2fb99125f2e7b31e
woragis <masteringthecode.woragis+github@gmail.com>
2025-01-14 13:31:48 -0300

    Frontend: update navbar model code formatting

--- frontend/src/components/Navbar/model.tsx
+++ frontend/src/components/Navbar/model.tsx
@@ -5,7 +5,6 @@ import { useTranslation } from "react-i18next";
 
 export const useNavbarModel = () => {
   const { t } = useTranslation();
-
   const auth = useSelector((state: RootState) => state.auth);
   const themeColors = useSelector((state: RootState) => state.theme.colors);
 
@@ -18,10 +17,12 @@ export const useNavbarModel = () => {
     { title: t("login.title"), path: Pages.Login },
     { title: t("register.title"), path: Pages.Register },
   ];
+
   const loggedLinks: NavPages[] = [
     { title: t("profile.title"), path: Pages.Profile },
     { title: t("logout.title"), path: Pages.Logout },
   ];
+
   const authLinks = auth.loggedIn ? loggedLinks : unloggedLinks;
 
   const navLogo = "";
