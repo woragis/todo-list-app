597fd12ca149a90eb5fbaac0bb2abf9261983e2d
woragis <masteringthecode.woragis+github@gmail.com>
2025-01-23 17:25:05 -0300

    Mobile: remove old files

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 // import 'package:todo_mobile/bloc/counter_cubit.dart';
-import 'package:todo_mobile/bloc/todo_cubit.dart';
+import 'package:todo_mobile/presentation/bloc/todo_cubit.dart';
 // import 'package:todo_mobile/screens/home_page.dart';
 // import 'package:todo_mobile/screens/counter_page.dart';
-import 'package:todo_mobile/screens/todos_page.dart';
+import 'package:todo_mobile/presentation/pages/todos_page.dart';
 
 void main() {
   runApp(BlocProvider(
