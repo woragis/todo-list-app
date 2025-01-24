cefdfdfa6f16e02485daff63e0a19ee1179ced12
woragis <masteringthecode.woragis+github@gmail.com>
2025-01-24 17:57:47 -0300

    Mobile: create login page

---
+++ mobile/lib/presentation/pages/login_page.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_mobile/data/models/auth_model.dart';
+import 'package:todo_mobile/presentation/bloc/auth_bloc.dart';
+import 'package:todo_mobile/presentation/widgets/appbar_widget.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  void _submitForm(BuildContext context) {
+    final email = _emailController.text.trim();
+    final password = _passwordController.text.trim();
+
+    if (email.isEmpty || password.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Please fill in all fields')),
+      );
+      return;
+    }
+
+    // Dispatch an event to create a new todo
+    context.read<AuthBloc>().add(
+          LoginEvent(user: Login(email: email, password: password)),
+        );
+
+    // Navigate back after creating the todo
+    Navigator.of(context).pop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: AppBarWidget(
+          title: "Register",
+          icon: Icons.door_back_door,
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(labelText: "Email"),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(labelText: "Password"),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+                onPressed: () => _submitForm(context),
+                child: const Text("Login"))
+          ],
+        ),
+      ),
+    );
+  }
+}
