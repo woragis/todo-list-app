867802548d4ba95052b79605e386dac4168361fd
woragis <masteringthecode.woragis+github@gmail.com>
2025-01-27 11:35:29 -0300

    Mobile: update routes and bloc provider

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -3,9 +3,17 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:todo_mobile/data/providers/todo_api_provider.dart';
 import 'package:todo_mobile/data/providers/todo_db_provider.dart';
+import 'package:todo_mobile/data/providers/user_api_provider.dart';
+import 'package:todo_mobile/data/providers/user_db_provider.dart';
 import 'package:todo_mobile/data/repositories/todo_repository_impl.dart';
+import 'package:todo_mobile/data/repositories/user_repository_impl.dart';
 import 'package:todo_mobile/domain/repositories/todo_repository.dart';
+import 'package:todo_mobile/domain/repositories/user_repository.dart';
 import 'package:todo_mobile/presentation/bloc/todo_bloc.dart';
+import 'package:todo_mobile/presentation/bloc/user_bloc.dart';
+import 'package:todo_mobile/presentation/pages/create_todo_page.dart';
+import 'package:todo_mobile/presentation/pages/login_page.dart';
+import 'package:todo_mobile/presentation/pages/register_page.dart';
 import 'package:todo_mobile/presentation/pages/todos_page.dart';
 // import 'package:todo_mobile/presentation/pages/todo_list_page.dart';
 
@@ -92,19 +100,31 @@ import 'package:todo_mobile/presentation/pages/todos_page.dart';
 void main() async {
   sqfliteFfiInit();
   databaseFactory = databaseFactoryFfi;
+  final String baseUrl = 'http://127.0.0.1:8080';
   final db = await databaseFactory.openDatabase('path');
   final TodoRepository todoRepository = TodoRepositoryImpl(
-    apiProvider: TodoApiProvider(baseUrl: 'http://127.0.0.1:8080'),
+    apiProvider: TodoApiProvider(baseUrl: baseUrl),
     dbProvider: TodoDbProvider(db: db),
   );
+  final UserRepository userRepository = UserRepositoryImpl(
+      apiProvider: UserApiProvider(baseUrl: '$baseUrl/auth'),
+      dbProvider: UserDbProvider(db: db, tableName: 'users'));
 
-  runApp(MyApp(todoRepository: todoRepository));
+  runApp(MyApp(
+    todoRepository: todoRepository,
+    userRepository: userRepository,
+  ));
 }
 
 class MyApp extends StatelessWidget {
   final TodoRepository todoRepository;
+  final UserRepository userRepository;
 
-  const MyApp({super.key, required this.todoRepository});
+  const MyApp({
+    super.key,
+    required this.todoRepository,
+    required this.userRepository,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -113,10 +133,19 @@ class MyApp extends StatelessWidget {
         BlocProvider(
           create: (context) => TodoBloc(todoRepository),
         ),
+        BlocProvider(
+          create: (context) => UserBloc(userRepository),
+        ),
       ],
       child: MaterialApp(
         title: 'Todo App',
-        home: const TodosPage(),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => TodosPage(),
+          '/auth/login': (context) => LoginPage(),
+          '/auth/register': (context) => RegisterPage(),
+          '/create-todo': (context) => CreateTodoPage(),
+        },
       ),
     );
   }
