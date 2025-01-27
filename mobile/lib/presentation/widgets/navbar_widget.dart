d763e8ea92adaecdaa399ae056448204d4afc341
woragis <masteringthecode.woragis+github@gmail.com>
2025-01-27 12:51:43 -0300

    Mobile: create navigation bar

---
+++ mobile/lib/presentation/widgets/navbar_widget.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class NavbarWidget extends StatefulWidget {
+  const NavbarWidget({Key? key}) : super(key: key);
+
+  @override
+  _NavbarWidgetState createState() => _NavbarWidgetState();
+}
+
+class _NavbarWidgetState extends State<NavbarWidget> {
+  int _currentIndex = 0;
+  List<Widget> body = const [
+    Icon(Icons.home),
+    Icon(Icons.menu),
+    Icon(Icons.person),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: body[_currentIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (int newIndex) {
+          setState(() {
+            _currentIndex = newIndex;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            label: 'home',
+            icon: Icon(
+              Icons.person,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
