80de241eef378f1dd2d3e512f726180a2dc634fd
woragis <masteringthecode.woragis+github@gmail.com>
2025-02-10 18:38:56 -0300

    Infra: update deploy script

--- scripts/docker/deploy.fish
+++ scripts/docker/deploy.fish
@@ -1,9 +1,17 @@
 #!/usr/bin/fish
 
-docker build backend/ -t woragis/todos-backend
+# SHA=$(git rev-parse HEAD)
+set SHA $(git rev-parse HEAD)
 
-docker build frontend/ -t woragis/todos-frontend
+docker build backend/ -t woragis/todos-backend:latest -t woragis/todos-backend:$SHA
+docker build frontend/ -t woragis/todos-frontend:latest -t woragis/todos-frontend:$SHA
 
-docker push woragis/todos-backend
+docker push woragis/todos-backend:latest
+docker push woragis/todos-frontend:latest
 
-docker push woragis/todos-frontend
+docker push woragis/todos-backend:$SHA
+docker push woragis/todos-frontend:$SHA
+
+# kubectl apply -f k8s
+# kubectl set image deployments/frontend frontend=woragis/todos-frontend:$SHA
+# kubectl set image deployments/backend backend=woragis/todos-backend:$SHA
