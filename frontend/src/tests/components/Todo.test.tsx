6b2e7decaf4a21932406dd8746a1ebf384f11afe
woragis <masteringthecode.woragis+github@gmail.com>
2025-02-12 00:56:14 -0300

    Frontend: test: create Todo test

---
+++ frontend/src/tests/components/Todo.test.tsx
@@ -0,0 +1,29 @@
+import { describe, expect, test } from 'vitest'
+import { render, screen } from '../test-utils'
+import '@testing-library/jest-dom'
+import { TodoView } from '../../components/Todo/view'
+import { Todo as TodoType } from '../../types/todos.types'
+
+describe('TodoView Component', () => {
+  test('renders "Did not find todo" when todo is undefined', () => {
+    render(<TodoView todo={undefined} />)
+
+    expect(screen.getByText(/Did not find todo/i)).toBeInTheDocument()
+  })
+
+  test('renders todo details when provided', () => {
+    const mockTodo: TodoType = {
+      id: '1',
+      name: 'Test Todo',
+      description: 'This is a test todo',
+      author_id: '2',
+      created_at: '',
+    }
+
+    render(<TodoView todo={mockTodo} />)
+
+    expect(screen.getByText(/id: 1/i)).toBeInTheDocument()
+    expect(screen.getByText(/Title: Test Todo/i)).toBeInTheDocument()
+    expect(screen.getByText(/This is a test todo/i)).toBeInTheDocument()
+  })
+})
