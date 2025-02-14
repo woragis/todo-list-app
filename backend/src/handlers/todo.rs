342c2b8af919d10d7c50022dce92ca52278f90bb
woragis <masteringthecode.woragis+github@gmail.com>
2025-02-13 22:53:18 -0300

    Create todo handlers

--- backend/src/handlers/todo.rs
+++ backend/src/handlers/todo.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 
-use crate::models::user::{CreateUser, User};
+use crate::models::todo::{CreateTodo, Todo};
 use axum::{
     extract::{Path, State},
     http::StatusCode,
@@ -10,23 +10,24 @@ use tokio::sync::Mutex;
 use tokio_postgres::Client;
 use uuid::Uuid;
 
-static TABLE: &str = "users";
-static FIELDS: &str = "name, email, password";
-static UPDATE_FIELDS: &str = "name = $1, email = $2, password = $3";
+static TABLE: &str = "todos";
+static FIELDS: &str = "title, description, completed";
+static UPDATE_FIELDS: &str = "title = $1, description = $2, completed = $3";
 static FIELDS_INPUT: &str = "$1, $2, $3";
 
 /// **Create User**
-pub async fn create_user(
+pub async fn create_todo(
     State(db): State<Arc<Mutex<Client>>>,
-    Json(payload): Json<CreateUser>,
+    Json(payload): Json<CreateTodo>,
 ) -> Result<StatusCode, StatusCode> {
     let client = db.lock().await;
 
+    let stmt = format!(
+        "INSERT INTO {} ({}) VALUES ({})",
+        TABLE, FIELDS, FIELDS_INPUT
+    );
     let result = client
-        .execute(
-            "INSERT INTO users (name, email, password) VALUES ($1, $2, $3)",
-            &[&payload.name, &payload.email, &payload.password],
-        )
+        .execute(&stmt, &[&payload.title, &payload.description, &false])
         .await;
 
     match result {
@@ -36,50 +37,58 @@ pub async fn create_user(
 }
 
 /// **Read User**
-pub async fn get_user(
+pub async fn get_todo(
     State(db): State<Arc<Mutex<Client>>>,
     Path(id): Path<Uuid>,
-) -> Result<Json<User>, StatusCode> {
+) -> Result<Json<Todo>, StatusCode> {
     let client = db.lock().await;
 
+    let stmt = format!("SELECT * FROM {} WHERE id = $1", TABLE);
     let row = client
-        .query_one("SELECT * FROM users WHERE id = $1", &[&id])
+        .query_one(&stmt, &[&id])
         .await
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
-    let user = User::from_row(&row);
+    let user = Todo::from_row(&row);
 
     Ok(Json(user))
 }
 
 /// **Read Users**
-pub async fn get_users(
+pub async fn get_todos(
     State(db): State<Arc<Mutex<Client>>>,
-) -> Result<Json<Vec<User>>, StatusCode> {
+) -> Result<Json<Vec<Todo>>, StatusCode> {
     let client = db.lock().await;
 
+    let stmt = format!("SELECT * FROM {}", TABLE);
     let rows = client
-        .query("SELECT * FROM users", &[])
+        .query(&stmt, &[])
         .await
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
-    let users = rows.iter().map(|row| User::from_row(row)).collect();
+    let users = rows.iter().map(|row| Todo::from_row(row)).collect();
 
     Ok(Json(users))
 }
 
 /// **Update User**
-pub async fn update_user(
+pub async fn update_todo(
     State(db): State<Arc<Mutex<Client>>>,
     Path(id): Path<Uuid>,
-    Json(payload): Json<CreateUser>,
+    Json(payload): Json<Todo>,
 ) -> Result<StatusCode, StatusCode> {
     let client = db.lock().await;
 
+    let stmt = format!("UPDATE {} SET {} WHERE id = $4", TABLE, UPDATE_FIELDS);
     let result = client
         .execute(
-            "UPDATE users SET name = $1, email = $2, password = $3 WHERE id = $4",
-            &[&payload.name, &payload.email, &payload.password, &id],
+            &stmt,
+            &[
+                &payload.title,
+                &payload.description,
+                &payload.completed,
+                &id,
+            ],
         )
         .await;
 
@@ -91,15 +100,14 @@ pub async fn update_user(
 }
 
 /// **Delete User**
-pub async fn delete_user(
+pub async fn delete_todo(
     State(db): State<Arc<Mutex<Client>>>,
     Path(id): Path<Uuid>,
 ) -> Result<StatusCode, StatusCode> {
     let client = db.lock().await;
 
-    let result = client
-        .execute("DELETE FROM users WHERE id = $1", &[&id])
-        .await;
+    let stmt = format!("DELETE FROM {} WHERE id = $1", TABLE);
+    let result = client.execute(&stmt, &[&id]).await;
 
     match result {
         Ok(1) => Ok(StatusCode::OK),
