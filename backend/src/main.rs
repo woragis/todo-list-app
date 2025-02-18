f19eaa3b39762c37028b233441fd70c806507aae
woragis <masteringthecode.woragis+github@gmail.com>
2025-02-17 21:43:30 -0300

    Add cors config

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -7,6 +7,7 @@ mod utils;
 use axum::Router;
 use database::db::connect;
 use routes::{auth::auth_routes, profile::profile_routes, todo::todo_routes, user::user_routes};
+use tower_http::cors::{Any, CorsLayer};
 use std::sync::Arc;
 use tokio::{net::TcpListener, sync::Mutex};
 
@@ -18,12 +19,18 @@ async fn main() {
     let pool = connect().await.expect("Error in database connection");
     let client = Arc::new(Mutex::new(pool));
 
+    let cors = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods(Any)
+        .allow_headers(Any);
+
     let app = Router::new()
         .nest("/auth", auth_routes())
         .nest("/profile", profile_routes())
         .nest("/users", user_routes())
         .nest("/todos", todo_routes())
-        .with_state(client);
+        .with_state(client)
+        .layer(cors);
 
     let listener = TcpListener::bind(HOST).await.unwrap();
     axum::serve(listener, app).await.unwrap();
