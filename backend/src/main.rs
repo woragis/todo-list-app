5da3ae7566098d4a418ad917be4b329aca71604e
woragis <masteringthecode.woragis+github@gmail.com>
2025-02-21 16:01:37 -0300

    Update main.rs to actix

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,20 +4,25 @@ mod models;
 mod routes;
 mod utils;
 
+use std::sync::Arc;
+
 use actix_cors::Cors;
 use actix_web::{http::header::{AUTHORIZATION, CONTENT_TYPE}, web::Data, App, HttpServer};
 use database::{db::connect, tables::create_tables};
 use routes::{auth::auth_routes, profile::profile_routes, todo::todo_routes, user::user_routes};
-use utils::response::ApiError;
+use tokio::sync::Mutex;
 
-static HOST: &str = "0.0.0.0:8080";
+static HOST: &str = "0.0.0.0";
+static PORT: u16 = 8080;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenvy::dotenv().ok();
 
-    let client = connect().await.map_err(ApiError::from)?;
-    create_tables(&client);
+    let client = connect().await.expect("Error connecting to client");
+    let client = Arc::from(Mutex::from(client));
+
+    create_tables(&client).await.ok();
 
     HttpServer::new(move || {
         App::new()
@@ -34,7 +39,8 @@ async fn main() -> std::io::Result<()> {
             .service(todo_routes())
             .service(user_routes())
     })
-    .bind(("0.0.0.0", 8080))
+    .bind((HOST, PORT))
+    .expect("Could not")
     .run()
     .await
 }
