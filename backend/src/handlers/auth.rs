0a26701633df1250168b4a7e9f5590da83ea6e6d
woragis <masteringthecode.woragis+github@gmail.com>
2025-02-21 15:51:13 -0300

    Update auth handler to actix

--- backend/src/handlers/auth.rs
+++ backend/src/handlers/auth.rs
@@ -4,7 +4,7 @@ use crate::{
     models::auth::{AuthRequest, AuthResponse},
     utils::response::{ApiError, ApiResponse},
 };
-use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use actix_web::{http::StatusCode, web::{Data, Json}, HttpResponse};
 use tokio::sync::Mutex;
 use tokio_postgres::Client;
 
@@ -14,12 +14,12 @@ static FIELDS_INPUT: &str = "$1, $2, $3";
 
 /// **Login User**
 pub async fn login(
-    State(db): State<Arc<Mutex<Client>>>,
-    Json(payload): Json<AuthRequest>,
-) -> Result<impl IntoResponse, ApiError> {
+    client: Data<Arc<Mutex<Client>>>,
+    payload: Json<AuthRequest>,
+) -> Result<HttpResponse, ApiError> {
     println!("Login request received for email: {}", payload.email);
 
-    let client = db.lock().await;
+    let client = client.lock().await;
 
     let stmt = format!("SELECT * FROM {} WHERE email = $1", TABLE);
     let row = client
@@ -39,12 +39,12 @@ pub async fn login(
 
 /// **Register User**
 pub async fn register(
-    State(db): State<Arc<Mutex<Client>>>,
-    Json(payload): Json<AuthRequest>,
-) -> Result<impl IntoResponse, ApiError> {
+    client: Data<Arc<Mutex<Client>>>,
+    payload: Json<AuthRequest>,
+) -> Result<HttpResponse, ApiError> {
     println!("Register request received for email: {}", payload.email);
 
-    let client = db.lock().await;
+    let client = client.lock().await;
 
     let stmt = format!(
         "INSERT INTO {} ({}) VALUES ({}) RETURNING *",
