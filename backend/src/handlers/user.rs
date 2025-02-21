9e4812f8cb8163217a0ee4a1efe8d38962bd983c
woragis <masteringthecode.woragis+github@gmail.com>
2025-02-21 15:31:54 -0300

    Fix user handler

--- backend/src/handlers/user.rs
+++ backend/src/handlers/user.rs
@@ -1,14 +1,10 @@
+use core::panic;
 use std::sync::Arc;
 
+use actix_web::{http::StatusCode, web::{self, Data, Json, Path}, HttpResponse, Responder};
 use crate::{
     models::user::{CreateUser, UpdateUser, User},
     utils::response::{ApiError, ApiResponse},
-};
-use axum::{
-    extract::{Path, State},
-    http::StatusCode,
-    response::IntoResponse,
-    Json,
 };
 use tokio::sync::Mutex;
 use tokio_postgres::Client;
@@ -19,16 +15,15 @@ static FIELDS: &str = "name, email, password";
 static UPDATE_FIELDS: &str = "name = $1, email = $2, password = $3";
 static FIELDS_INPUT: &str = "$1, $2, $3";
 
-/// **Create User**
+
 pub async fn create_user(
-    State(db): State<Arc<Mutex<Client>>>,
-    Json(payload): Json<CreateUser>,
-) -> Result<impl IntoResponse, ApiError> {
-    let client = db.lock().await;
+    client: Data<Arc<Mutex<Client>>>,
+    payload: web::Json<CreateUser>,
+) -> Result<HttpResponse, ApiError> {
+    let client = client.lock().await;
 
     let stmt = format!(
-        "INSERT INTO {} ({}) VALUES ({}) RETURNING *",
-        TABLE, FIELDS, FIELDS_INPUT
+        "INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id, name, email"
     );
 
     let row = client
@@ -36,110 +31,129 @@ pub async fn create_user(
         .await
         .map_err(ApiError::from)?;
 
-    let user = User::from_row(&row);
+    let user = User {
+        id: row.get("id"),
+        name: row.get("name"),
+        email: row.get("email"),
+        password: String::new(), // Do not return the password
+    };
+
     Ok(ApiResponse::success(
         user,
         "User created successfully",
-        StatusCode::CREATED,
+        StatusCode::CREATED
     ))
 }
 
 /// **Read User**
 pub async fn get_user(
-    State(db): State<Arc<Mutex<Client>>>,
-    Path(id): Path<Uuid>,
-) -> Result<impl IntoResponse, ApiError> {
-    let client = db.lock().await;
+    client: Data<Arc<Mutex<Client>>>,
+    user_id: Path<Uuid>,
+) -> Result<HttpResponse, ApiError> {
+    let client = client.lock().await;
 
-    let stmt = format!("SELECT * FROM {} WHERE id = $1", TABLE);
+    let user_id = user_id.to_string();
+
+    let stmt = "SELECT id, name, email FROM users WHERE id = $1";
     let row = client
-        .query_one(&stmt, &[&id])
+        .query_one(stmt, &[&user_id])
         .await
         .map_err(ApiError::from)?;
 
-    let user = User::from_row(&row);
+    let user = User {
+        id: row.get("id"),
+        name: row.get("name"),
+        email: row.get("email"),
+        password: String::new(), // Do not return the password
+    };
+
     Ok(ApiResponse::success(
         user,
         "User retrieved successfully",
-        StatusCode::OK,
+        StatusCode::OK
     ))
 }
 
+
 /// **Read Users**
-pub async fn get_users(
-    State(db): State<Arc<Mutex<Client>>>,
-) -> Result<impl IntoResponse, ApiError> {
-    let client = db.lock().await;
+pub async fn get_users(client: web::Data<Arc<Mutex<Client>>>) -> Result<HttpResponse, ApiError> {
+    let client = client.lock().await;
 
-    let stmt = format!("SELECT * FROM {}", TABLE);
-    let rows = client.query(&stmt, &[]).await.map_err(ApiError::from)?;
+    let stmt = "SELECT id, name, email FROM users";
+    let rows = client
+        .query(stmt, &[])
+        .await
+        .map_err(ApiError::from)?;
 
-    let users: Vec<User> = rows.iter().map(User::from_row).collect();
+    let users: Vec<User> = rows
+        .iter()
+        .map(|row| User {
+            id: row.get("id"),
+            name: row.get("name"),
+            email: row.get("email"),
+            password: String::new(), // Do not return the password
+        })
+        .collect();
+
     Ok(ApiResponse::success(
         users,
         "Users retrieved successfully",
-        StatusCode::OK,
+        StatusCode::OK
     ))
 }
 
+
 /// **Update User**
 pub async fn update_user(
-    State(db): State<Arc<Mutex<Client>>>,
-    Path(id): Path<Uuid>,
-    Json(payload): Json<UpdateUser>,
-) -> Result<impl IntoResponse, ApiError> {
-    let client = db.lock().await;
+    client: Data<Arc<Mutex<Client>>>,
+    user_id: Path<Uuid>,
+    payload: Json<UpdateUser>
+) -> impl Responder {
+    let client = client.lock().await;
+
+    let user_id = user_id.to_string();
 
     let stmt = format!("UPDATE {} SET {} WHERE id = $4", TABLE, UPDATE_FIELDS);
     let result = client
-        .execute(
-            &stmt,
-            &[&payload.name, &payload.email, &payload.password, &id],
-        )
+        .execute(&stmt, &[&payload.name, &payload.email, &payload.password, &user_id])
         .await
-        .map_err(ApiError::from)?;
+        .map_err(ApiError::from);
 
-    if result == 1 {
-        let updated_user = User {
-            id,
-            name: payload.name,
-            email: payload.email,
-            password: payload.password,
-        };
-        return Ok(ApiResponse::success(
-            updated_user,
+    match result {
+        Ok(1) => ApiResponse::success(
+            user_id.to_string(),
             "User updated successfully",
             StatusCode::OK,
-        ));
-    } else if result == 0 {
-        return Err(ApiError::Custom("User not found on update".to_string()));
+        ),
+        _ => panic!("Error in update user")
+        // Ok(_) => ApiError::Custom("User not found".to_string()),
+        // Err(e) => ApiError::Custom("o".to_string())
+        // _ => ApiError::Custom("Unexpected delete count".to_string()),
     }
-
-    Err(ApiError::Custom("Unexpected update count".to_string()))
 }
-
 /// **Delete User**
 pub async fn delete_user(
-    State(db): State<Arc<Mutex<Client>>>,
-    Path(id): Path<Uuid>,
-) -> Result<impl IntoResponse, ApiError> {
-    let client = db.lock().await;
+    client: Data<Arc<Mutex<Client>>>,
+    user_id: Path<Uuid>,
+) -> impl Responder {
+    let client = client.lock().await;
+
+    let user_id = user_id.to_string();
 
     let stmt = format!("DELETE FROM {} WHERE id = $1", TABLE);
     let result = client
-        .execute(&stmt, &[&id])
+        .execute(&stmt, &[&user_id])
         .await
-        .map_err(ApiError::from)?;
+        .map_err(ApiError::from);
 
-    if result == 1 {
-        return Ok(ApiResponse::success(
-            id.to_string(),
+    match result {
+        Ok(1) => ApiResponse::success(
+            user_id.to_string(),
             "User deleted successfully",
             StatusCode::OK,
-        ));
-    } else if result == 0 {
-        return Err(ApiError::Custom("User not found".to_string()));
+        ),
+        _ => panic!("Error in update user")
+        // Ok(0) => ApiError::Custom("User not found".to_string()),
+        // _ => ApiError::Custom("Unexpected delete count".to_string()),
     }
-
-    Err(ApiError::Custom("Unexpected delete count".to_string()))
 }
