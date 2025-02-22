0fcb0da931a2e9e06cec6aca90a1c4901b37af48
woragis <masteringthecode.woragis+github@gmail.com>
2025-02-22 13:49:44 -0300

    Add redis successfull connection log

--- backend/src/database/cache.rs
+++ backend/src/database/cache.rs
@@ -5,6 +5,8 @@ use deadpool_redis::{Config, Pool};
 
 pub fn pool() -> Pool {
     let redis_url = env::var("REDIS_URL").expect("Error fetching redis url");
+    println!("Redis url: {}", redis_url);
+
     let redis_config = Config::from_url(redis_url);
     let redis_pool = redis_config.create_pool(None).expect("Failed to create redis pool");
     redis_pool
