74347561b514e4f24315b4cf66fa59c85de1b5b2
woragis <masteringthecode.woragis+github@gmail.com>
2025-02-22 00:24:41 -0300

    Fix profile routes conflict

--- backend/src/routes/profile.rs
+++ backend/src/routes/profile.rs
@@ -5,6 +5,6 @@ use crate::handlers::profile::{delete_user_profile, get_user_profile, update_use
 pub fn profile_routes() -> Scope {
     scope("/profile")
     .route("/", get().to(get_user_profile))
-    .route("/", put().to(update_user_profile))
-    .route("/", delete().to(delete_user_profile))
+    .route("/update", put().to(update_user_profile))
+    .route("/delete", delete().to(delete_user_profile))
 }
