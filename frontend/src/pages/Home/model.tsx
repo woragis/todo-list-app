2295d705cd9b5381ceeae76bb789bb02cd4fadeb
woragis <masteringthecode.woragis+github@gmail.com>
2025-02-24 14:52:54 -0300

    Update login and home page

--- frontend/src/pages/Home/model.tsx
+++ frontend/src/pages/Home/model.tsx
@@ -2,8 +2,14 @@ import { useTranslation } from 'react-i18next'
 
 import { useAppSelector } from '@/features/hooks'
 import { useGetTodosQuery } from '@/features/todos/apiSlice'
+import { useNavigate } from '@tanstack/react-router'
 
 export const useHomeModel = () => {
+  const navigate = useNavigate()
+
+  const { logged } = useAppSelector((state) => state.auth)
+  if (!logged) navigate({ to: '/auth/login' })
+
   const { t } = useTranslation()
   const todosTitle = t('todos.title')
   const todosNotFound = t('todos.not-found')
