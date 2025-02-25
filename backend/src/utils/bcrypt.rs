ca7c29fcc044a048e7668ad4e0e9ab0644690a57
woragis <masteringthecode.woragis+github@gmail.com>
2025-02-25 17:58:32 -0300

    Add debug and error logging to bcrypt

--- backend/src/utils/bcrypt.rs
+++ backend/src/utils/bcrypt.rs
@@ -1,15 +1,28 @@
 use bcrypt::{hash, verify, BcryptError};
+use log::{debug, error};
 
 pub fn hash_password(password: &str) -> Result<String, BcryptError> {
     match hash(password, 12) {
-        Ok(hash) => Ok(hash),
-        Err(e) => Err(e)
+        Ok(hash) => {
+            debug!("Hashed password: {} to {}", password, hash);
+            Ok(hash)
+        },
+        Err(e) => {
+            error!("Bcrypt error: {}", e);
+            Err(e)
+        }
     }
 }
 
 pub fn compare_password(password: &str, hash: &str) -> Result<bool, BcryptError> {
     match verify(password, hash) {
-        Ok(is_equal) => Ok(is_equal),
-        Err(e) => Err(e),
+        Ok(is_equal) => {
+            debug!("comparing password... is equal?: {}", is_equal);
+            Ok(is_equal)
+        },
+        Err(e) => {
+            error!("Bcrypt error: {}", e);
+            Err(e)
+        },
     }
 }
